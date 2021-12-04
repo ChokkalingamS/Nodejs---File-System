ae21d6a11239688f77049a10679c797691910f7b
Chokkalingam S <lingam1707@gmail.com>
2021-12-04 17:03:38 +0530

    version1.0

---
+++ file.js
@@ -0,0 +1,41 @@
+import express from "express";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config(); // It returns all the key in process.env
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Homepage
+app.get("/homepage", (request, response) => {
+  response.send({ msg: "Nodejs File System" });
+});
+
+// Creating files
+app.get("/createfile", (request, response) => {
+  let date = new Date();
+  date = date.toISOString().replace(/\:/g, "-");
+  console.log(date);
+  fs.writeFile(`Files/${date}.txt`, date, () => {
+    console.log("File created");
+  });
+  response.send({ msg: "File Created" });
+});
+
+// Geting Files
+app.get("/getfiles", (request, response) => {
+  fs.readdir("Files", (err, files) => {
+    if (err) {
+      console.log(err);
+      return response.status(404).send(err);
+    } else {
+      return response.send(files);
+    }
+  });
+});
+
+// Server Port
+app.listen(PORT, () => {
+  console.log("Server Started in", PORT);
+});
